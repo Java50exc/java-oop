f3d56a79668cd29fe0c90517f9685c79bc4d1fe0
allanteone <allanteone@gmail.com>
2024-02-22 20:31:58 +0200

    hw-08 final

--- src/telran/exceptions/test/ExceptionsTest.java
+++ src/telran/exceptions/test/ExceptionsTest.java
@@ -11,46 +11,55 @@ class ExceptionsTest {
 
 	@Test
 	@Disabled
-	void testException()  {
+	void testException() {
 		int res = 0;
-			try {
-				res = itThrowsCheckedException(10000);
-				
-				System.out.println("everything ok");
-			} catch (RuntimeException e) {
-				System.out.println(e.getMessage());
-				res = 100;
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-				res = 200;
-			}
+		try {
+			res = itThrowsCheckedException(10000);
 
-			
+			System.out.println("everything ok");
+		} catch (RuntimeException e) {
+			System.out.println(e.getMessage());
+			res = 100;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			res = 200;
+		}
+
 	}
-	private int itThrowsCheckedException(int number) throws Exception  {
-		if(number < 0) {
+
+	private int itThrowsCheckedException(int number) throws Exception {
+		if (number < 0) {
 			throw new Exception("just test checked exception");
 		}
 		if (number > 1000) {
 			throw new RuntimeException("number cannot be greater than 1000");
 		}
 		return number * 2;
-		
+
 	}
+
 	@Test
 	void ballBrokenFloorTest() {
 		BallBrokenFloor bbf = new BallBrokenFloor(200);
-		assertEquals(bbf.getFloor(),getMinFloor(bbf));
+		assertEquals(bbf.getFloor(), getMinFloor(bbf));
 	}
+
 	private int getMinFloor(BallBrokenFloor bbf) {
-		for (int i = 0; i < 200; i++) {
+		int left = 1;
+		int right = 200;
+		int middle = right / 2;
+
+		while (left < right) {
 			try {
-				bbf.broken(i);
+				bbf.broken(middle);
+				left = middle + 1;
 			} catch (Exception e) {
-				return i;
+				right = middle;
+			} finally {
+				middle = (left + right) / 2;
 			}
 		}
-		return -1;
+		return middle;
 	}
 
 }
